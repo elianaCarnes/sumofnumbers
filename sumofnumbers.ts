1b8584d04a1739b2b9cadca9cab1612e844e7993
elianaCarnes <eliana.carnes@gmail.com>
2025-02-09 22:56:35 -1000

    Update sumofnumbers.ts

--- sumofnumbers.ts
+++ sumofnumbers.ts
@@ -1,26 +1,31 @@
 function sumFor(numbers: number[]): number {
-  let sum = 0;
-  for (const num of numbers) {
-    sum += num;
-  }
-  return sum;
+    let sum = 0;
+    for (const num of numbers) {
+        sum += num;
+    }
+    return sum;
 }
 
 function sumWhile(numbers: number[]): number {
-  let sum = 0;
-  let i = 0;
-  while (i < numbers.length) {
-    sum += numbers[i];
-    i++;
-  }
-  return sum;
+    let sum = 0;
+    let i = 0;
+    while (i < numbers.length) {
+        sum += numbers[i];
+        i++;
+    }
+    return sum;
 }
 
 function sumRecursion(numbers: number[], index = 0): number {
-  if (index >= numbers.length) return 0;
-  return numbers[index] + sumRecursion(numbers, index + 1);
+    if (index >= numbers.length) return 0;
+    return numbers[index] + sumRecursion(numbers, index + 1);
 }
 
-console.log(sumFor([1, 2, 3, 4])); // Output: 10
-console.log(sumWhile([1, 2, 3, 4])); // Output: 10
-console.log(sumRecursion([1, 2, 3, 4])); // Output: 10
+function sumTheFunctionalWay(numbers: number[]): number {
+    return numbers.reduce((acc, num) => acc + num, 0);
+}
+
+console.log(sumFor([1, 2, 3, 4]));
+console.log(sumWhile([1, 2, 3, 4]));
+console.log(sumRecursion([1, 2, 3, 4]));
+console.log(sumTheFunctionalWay([1, 2, 3, 4]));
